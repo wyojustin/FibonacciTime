e9ae2983df62496574379ec544b9e61768bbeea7
Justin Shaw <wyojustin@gmail.com>
2020-08-29 08:31:14 -0400

    ESP32 is resetting after adding set_timezone_from_ip

--- arduino/FibonacciTime/FibonacciTime.ino
+++ arduino/FibonacciTime/FibonacciTime.ino
@@ -1,4 +1,14 @@
+#include <Time.h>
+#include <WiFiManager.h>
+#include <HTTPClient.h>
+#include <NTPClient.h>
+#include <EEPROM.h>
+#include <EEPROMAnything.h>
+
+#include "credentials.h"
 #include "FastLED.h"
+#include "get_time.h"
+#include "config.h"
 
 FASTLED_USING_NAMESPACE
 
@@ -17,7 +27,7 @@ FASTLED_USING_NAMESPACE
 
 
 
-#define DATA_PIN    21
+#define DATA_PIN    4 //32 //21
 #define LED_TYPE    WS2812
 #define pi         3.14159
 //#define CLK_PIN   19
@@ -33,6 +43,42 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS          255
 #define FRAMES_PER_SECOND  120
 
+WiFiManager wifiManager;
+
+NTPClock ntp_clock;
+DS3231Clock ds3231_clock;
+DoomsdayClock doomsday_clock;
+struct config_t{
+  int timezone;
+  uint8_t brightness;
+  uint8_t display_idx;
+  bool factory_reset;
+  bool use_wifi;
+  bool use_ip_timezone;
+  byte mqtt_ip[4];
+  bool flip_display;
+  uint32_t last_tz_lookup; // look up tz info every Sunday at 3:00 AM
+  uint8_t solid_color_rgb[3];
+  bool use_ntp_time;
+  bool wifi_reset;
+  uint8_t faceplate_idx;
+} config;
+
+
+void wifi_setup(){
+  if(config.wifi_reset){
+    config.wifi_reset = false;
+    saveSettings();
+    wifiManager.startConfigPortal("KLOK");
+  }
+  else{
+    wifiManager.autoConnect("KLOK");
+  }
+  Serial.println("Yay connected!");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("WyoLum.com!");
@@ -43,6 +89,8 @@ void setup() {
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.setMaxPowerInMilliWatts(2000);
+  wifi_setup();
+  //set_timezone_from_ip();
 }
 
 
@@ -174,13 +222,136 @@ void simple_clock(){
   unsigned long long t;
   uint16_t ms;
   
-  t = 100 * millis()  / 1000 + 12000;
+  t = 1 * millis()  / 1000 + 12000;
   ms = millis() % 1000;
   off();
   draw_hour_hand(t);
   draw_minute_hand(t);
   //draw_seconds(t, ms);
+  delay(100);
 }
+
+// return json value for name identified (or "not found")
+String jsonLookup(String s, String name){
+  String out;
+  int start, stop;
+  
+  name = String("\"") + name + String("\""); // add bounding quotes
+
+  start = s.indexOf(name);
+  if(start < 0){
+    out = String("not found");
+  }
+  else{
+    start +=  name.length() + 2;
+    stop = s.indexOf('"', start);
+    out = s.substring(start, stop);
+  }
+  return out;
+}
+
+void set_timezone_offset(int32_t offset){
+  config.timezone = offset % 86400;
+  saveSettings();
+  if(config.use_wifi){
+    ntp_clock.setOffset(config.timezone);
+  }
+}
+
+void set_timezone_from_ip(){
+
+  HTTPClient http;
+  String payload;
+  
+  Serial.print("[HTTP] begin...\n");
+  // configure traged server and url
+  //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+  // http.begin("http://example.com/index.html"); //HTTP
+
+  //http.begin("https://timezoneapi.io/api/ip");// no longer works!
+  String url = String("https://www.wyolum.com/utc_offset/utc_offset.py") +
+    String("?refresh=") + String(millis()) +
+    String("&localip=") +
+    String(WiFi.localIP()[0]) + String('.') + 
+    String(WiFi.localIP()[1]) + String('.') + 
+    String(WiFi.localIP()[2]) + String('.') + 
+    String(WiFi.localIP()[3]) + String('&') +
+    String("macaddress=") + WiFi.macAddress() + String('&') + 
+    String("dev_type=ClockIOT");
+  Serial.println(url);
+  http.begin(url);
+  
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+  
+  // httpCode will be negative on error
+  //httpCode = -1; // force error
+  if(httpCode < 0){
+    http.end();
+    url = String("https://ipapi.co/json/?key=");
+    Serial.print("Using backup url:");
+    Serial.println(url + "<SECRET KEY>");
+    http.begin(url + ipapikey);
+  
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    httpCode = http.GET();
+  }
+  payload = http.getString();
+  if(httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    
+    // file found at server
+    //String findme = String("offset_seconds");
+    if(httpCode == HTTP_CODE_OK) {
+      Serial.print("payload:");
+      Serial.println(payload);
+      payload.replace(" ", "");
+      String offset_str = jsonLookup(payload, String("utc_offset"));
+      int hours = offset_str.substring(0, 3).toInt();
+      int minutes = offset_str.substring(3, 5).toInt();
+      if(hours < 0){
+	minutes *= -1;
+      }
+      int offset = hours * 3600 + minutes * 60;
+
+      String utc_str =jsonLookup(payload, String("utc"));
+      Serial.print("  UTC:"); Serial.println(utc_str);
+      if(!utc_str.equals("not found")){
+	uint32_t local = utc_str.toInt() + offset;
+	Serial.print("Local:");Serial.println(local);
+	ds3231_clock.set(local);
+      }
+      if(doomsday_clock.master->initialized){
+	Serial.println("NTP is clock alive!");
+      }
+      else{
+	Serial.println("No NTP, fall back to DS3231");
+      }
+      Serial.println();
+      if(config.use_ip_timezone){
+	Serial.print("timezone_offset String:");
+	Serial.println(offset_str);
+	Serial.print("timezone_offset:");
+	Serial.println(offset);
+	set_timezone_offset(offset);
+	config.last_tz_lookup = doomsday_clock.gmt();
+	saveSettings();
+      }
+      else{
+	Serial.println("Using previously selected timezone");
+      }
+    }
+    else{
+      Serial.println("No timezone found");
+      Serial.println("Payload:");
+      Serial.println(payload);
+    }
+  }
+}
+
 void clock_test(){
   unsigned long long i, j, k, led, hh, mm, ss;
   uint32_t now = millis();
@@ -191,10 +362,8 @@ void clock_test(){
       draw_right_arm(i);
       FastLED.show();
       Serial.println((int)i);
-      delay(10000);
+      delay(100);
     }
-  }
-  while(1){
     for(i = 0; i < 3600 * 2; i+= 60){
       off();
       draw_minute_hand(i);
@@ -220,3 +389,12 @@ void clock_test(){
   }
 }
 
+
+void loadSettings(){
+  EEPROM_readAnything(0, config);
+}
+
+void saveSettings(){
+  EEPROM_writeAnything(0, config);
+  EEPROM.commit();
+}
